f61f3cf6ced940df5526a6bce4316754ad1ef798
Jonasz <jonaszgrudzien1234@gmail.com>
2025-11-07 15:37:58 +0000

    Completed ContactForm

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,31 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit} >
+      <label>Enter your Name:
+        <input 
+        type="text" 
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required/>
+      </label>
+      <label>Enter your Email:
+        <input 
+        type="email" 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required/>
+      </label>
+      <label>Enter your Phone Number:
+        <input 
+        type="tel" 
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        pattern="^((?:0|\+44)\s?\d{4}\s?\d{6})$" 
+        required/>
+      </label>
+        <input type="submit"></input>
+    </form>
   );
 };
 
