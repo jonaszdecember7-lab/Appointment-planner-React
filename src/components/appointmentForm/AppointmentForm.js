6d3dc7f510e138184baf8f8d370ca93d50f5587a
Jonasz <jonaszgrudzien1234@gmail.com>
2025-11-10 16:23:34 +0000

    Completed appointment form

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 const getTodayString = () => {
   const [month, day, year] = new Date()
@@ -8,6 +9,8 @@ const getTodayString = () => {
 };
 
 export const AppointmentForm = ({
+  name,
+  setName,
   contacts,
   title,
   setTitle,
@@ -21,6 +24,38 @@ export const AppointmentForm = ({
 }) => {
 
   return (
-    <></>
+    <form onSubmit={handleSubmit} >
+      <label>Enter your Name:
+        <input 
+        type="text" 
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required/>
+      </label>
+
+      <ContactPicker
+      contacts={contacts}
+      value={contact}
+       onChange={(e) => setContact(e.target.value)}/>
+
+      <label>Pick a date:
+        <input 
+        type="date" 
+        value={date}
+        min={getTodayString()}
+        onChange={(e) => setDate(e.target.value)}
+        required/>
+      </label>
+
+      <label>Choose a time:
+        <input 
+        type="time" 
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+        required/>
+      </label>
+      
+        <input type="submit"></input>
+    </form>
   );
 };
